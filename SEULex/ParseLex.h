e8e71342fdb2b4f8ccd7d91d0805e8f19e63d750
diand1an <diand1an@users.noreply.github.com>
2020-06-04 11:45:16 +0800

    SEULex

---
+++ SEULex/ParseLex.h
@@ -0,0 +1,20 @@
+//读入并解析lex.l文件
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+
+//字符串分割函数
+vector<string> split();
+//去除字符串的首尾空格
+string &trim(string &a);
+
+void readAndParseLex(vector<string>&P1,map<string,tring>&reMap,
+vector<Rules>&rules,vectior<string>&P4){
+
+}
